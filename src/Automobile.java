6ebfa6b589083eaec8e1335b4dab08ad53178a0a
Carlo Scocco <carlo.scocco@einsteinvimercate.edu.it>
2024-10-13 18:32:22 +0200

    first commit

---
+++ src/Automobile.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Automobile implements Runnable {
+
+    private String nome;
+    private ParcheggioConPostiRiservati parcheggio;
+    
+    public Automobile(String nome, ParcheggioConPostiRiservati parcheggio) {
+        this.nome = nome;
+        this.parcheggio = parcheggio;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            final Random RANDOM = new Random();
+
+            TimeUnit.SECONDS.sleep(RANDOM.nextInt(2));
+
+            // richiesta parcheggio
+            System.out.println(this.toString() + " sta richiedendo un posto");
+            Posto POSTO = parcheggio.richiedi();
+
+            // utilizzo parcheggio
+            System.out.println(this.toString() + " sta sostando nel posto " + POSTO.toString());
+            TimeUnit.SECONDS.sleep(RANDOM.nextInt(5));
+
+            // rilascio parcheggio
+            System.out.println(this.toString() + " sta liberando il posto " + POSTO.toString());
+            parcheggio.rilascia(POSTO);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Automobile " + nome;
+    }
+
+}
---
+++ src/ParcheggioConPostiRiservati.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class ParcheggioConPostiRiservati { // realizzato con un Monitor
+
+    final private ArrayList<Posto> POSTI_DISPONIBILI = new ArrayList<Posto>();
+
+    public ParcheggioConPostiRiservati (int numeroPosti){
+
+        for (int numero = 1; numero <= numeroPosti; numero++){
+            final Posto POSTO = new Posto(numero);
+            POSTI_DISPONIBILI.add(POSTO);
+        }            
+    }
+
+    public synchronized Posto richiedi() throws InterruptedException{
+        
+        while (POSTI_DISPONIBILI.size() == 0)
+            wait();
+        
+        return POSTI_DISPONIBILI.removeFirst();
+    }
+
+    public synchronized void rilascia(Posto posto){
+        POSTI_DISPONIBILI.add(posto);
+        notify();
+    }
+
+}
