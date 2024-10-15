9d0cbd156d27c1ecbaaa1a69c8dd6b5708288867
Carlo Scocco <carlo.scocco@einsteinvimercate.edu.it>
2024-10-15 08:00:57 +0200

    modificato da Dijkstra a Mutex

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     
     public static void main(String[] args) throws Exception {
-             
-        final int NUMERO_POSTI_DEL_PARCHEGGIO = 4;
 
-        final Parcheggio PARCHEGGIO = new Parcheggio(NUMERO_POSTI_DEL_PARCHEGGIO);
+        final Parcheggio PARCHEGGIO = new Parcheggio(Parcheggio.MUTEX);
         
         final int NUMERO_VEICOLI = 10;
         for (int i=1; i <= NUMERO_VEICOLI; i++)
